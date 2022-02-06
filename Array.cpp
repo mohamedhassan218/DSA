68602afa508611b5536c7b86d2f20fb2a1236794
mohamedhassan218 <m.hassan.def@gmail.com>
2022-02-06 18:32:22 +0200

    First problem solving on Hackerrank

---
+++ Array.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int numberOfElements;
+    cin >> numberOfElements;
+    int myArray[numberOfElements];
+    
+    for (int i = 0; i < numberOfElements; i++)
+    {
+        cin >> myArray[i];
+    }
+    
+    for (int i = (numberOfElements - 1); i >= 0; i--)
+    {
+        cout << myArray[i] << " ";
+    }
+    return 0;
+}
