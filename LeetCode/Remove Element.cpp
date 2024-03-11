5ff93c7bc939096e773512e353db949b3eb006b0
mohamedhassan218 <m.hassan.def@gmail.com>
2024-03-11 08:19:37 +0200

    Solve new problem!

---
+++ LeetCode/Remove Element.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+class Solution {
+public:
+    int removeElement(std::vector<int>& nums, int val) {
+        int n = nums.size();
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != val) {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
+};
